28d5041af4cf8f3065c6ecfa51e263ff0d4a5429
tanphat24 <huynhtanphat.24112001@gmail.com>
2023-10-24 16:10:33 +0700

    final version 24/10/2023

---
+++ my-CV/js/script.js
@@ -0,0 +1,31 @@
+const stars = document.querySelectorAll(".star");
+
+stars.forEach((star, index) => {
+  star.addEventListener("click", () => {
+    stars.forEach((star, index2) => {
+      index2 <= index ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+const star1 = document.querySelectorAll(".star1");
+
+star1.forEach((star, index) => {
+  star.addEventListener("click", () => {
+    star1.forEach((star, index2) => {
+      index2 <= index ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+const star2 = document.querySelectorAll(".star2");
+
+star2.forEach((star, index) => {
+  star.addEventListener("click", () => {
+    star2.forEach((star, index2) => {
+      index2 <= index ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+
