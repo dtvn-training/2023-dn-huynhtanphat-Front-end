0c4cd0b067e401497484a1ae911ee32a6f52ed5b
HuynhTanPhat <huynhtanphat.24112001@gmail.com>
2023-10-26 21:54:35 +0700

    update version3 26/10/2023

--- my-CV/js/script.js
+++ my-CV/js/script.js
@@ -1,34 +1,34 @@
-// const stars = document.querySelectorAll(".star");
+const stars = document.querySelectorAll(".star");
 
-// stars.forEach((star, index) => {
-//   star.addEventListener("click", () => {
-//     stars.forEach((star, index2) => {
-//       index2 <= index ? star.classList.add("active") : star.classList.remove("active");
-//     });
-//   });
-// });
+stars.forEach((star, index) => {
+  star.addEventListener("click", () => {
+    stars.forEach((star, index2) => {
+      index2 <= index ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
 
-// const star1 = document.querySelectorAll(".star1");
+const star1 = document.querySelectorAll(".star1");
 
-// star1.forEach((star, index) => {
-//   star.addEventListener("click", () => {
-//     star1.forEach((star, index2) => {
-//       index2 <= index ? star.classList.add("active") : star.classList.remove("active");
-//     });
-//   });
-// });
+star1.forEach((star, index) => {
+  star.addEventListener("click", () => {
+    star1.forEach((star, index2) => {
+      index2 <= index ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
 
-// const star2 = document.querySelectorAll(".star2");
-// let index2;
-// star2.forEach((star, index) => {
-//   console.log(index);
-//   star.addEventListener("click", () => {
+const star2 = document.querySelectorAll(".star2");
+let index2;
+star2.forEach((star, index) => {
+  console.log(index);
+  star.addEventListener("click", () => {
     
-//     star2.forEach((star, index2) => {
-//       console.log(index2.valueOf);
-//       index2 <= index ? star.classList.add("active") : star.classList.remove("active");
-//     });
-//   });
-// });
+    star2.forEach((star, index2) => {
+      console.log(index2.valueOf);
+      index2 <= index ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
 
 
